b669c263ac9c49aa1cf3ad67e4c8f83b78f46d4e
Sohan Samant <sohansamant@outlook.com>
2021-05-17 12:32:36 +0530

    Initial Commit

---
+++ ProjectOneTestingApp/ProjectOneTestingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectOneTestingAppApp.swift
+//  ProjectOneTestingApp
+//
+//  Created by Sohan Samant on 5/17/21.
+//
+
+import SwiftUI
+
+@main
+struct ProjectOneTestingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
